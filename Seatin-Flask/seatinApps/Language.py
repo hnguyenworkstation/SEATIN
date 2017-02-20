627ec8907ace706919049ca181316e2beb1612e8
tanthanhhoang <tanthanh_hoang@student.uml.edu>
2017-02-20 14:16:50 -0500

    Add server-side code

---
+++ Seatin-Flask/seatinApps/Language.py
@@ -0,0 +1,7 @@
+en = {
+	'SEARCH'      : 'Search'
+	'FIRST_NAME'  : 'First name'
+	'MIDDLE_NAME' : 'Middle name'
+	'LAST_NAME'   : 'Last name'
+	'EMAIL'       : 'Email
+}
---
+++ Seatin-Flask/seatinApps/seatinApps.py
@@ -0,0 +1,9 @@
+#flask frame-work
+from flask import Flask, render_template, session, redirect, url_for
+
+#mysql database
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+
